29a9acf301e4684016b2cb07e07bd9279ff7db58
Epharos <neox30650@gmail.com>
2024-11-17 02:09:49 +0100

    Added precompiled header

---
+++ Core/src/pch.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+#include <array>
+#include <list>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#include <thread>
+
+#include "Util/Logger.hpp"
