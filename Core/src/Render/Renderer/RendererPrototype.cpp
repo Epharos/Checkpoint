021dc4bbbbe5a7beef84dae1eb1748c5ee472dc2
Aurelien REY (Epharos) <neox30650@gmail.com>
2025-11-13 16:15:28 +0100

    Added the RendererPrototype class

---
+++ Core/src/Render/Renderer/RendererPrototype.cpp
@@ -0,0 +1,143 @@
+#include "pch.hpp"
+#include "RendererPrototype.hpp"
+
+void cp::RendererPrototype::CreateFixedPipeline() {}
+void cp::RendererPrototype::CreateRenderPasses() {}
+
+uint32_t cp::RendererPrototype::PrepareFrame(cp::Swapchain* _swapchain)
+{
+	if (!_swapchain)
+	{
+		LOG_ERROR("Swapchain is not initialized, cannot prepare frame");
+		return ~0u;
+	}
+
+	if (_swapchain->GetCurrentFrameIndex() >= _swapchain->GetFrameCount())
+	{
+		LOG_WARNING("Current frame index is out of bounds, resetting to 0");
+		_swapchain->SetCurrentFrame(0);
+		return -1;
+	}
+
+	if (context->GetDevice().waitForFences(1, &_swapchain->GetCurrentFrame()->GetInFlightFence(), VK_TRUE, std::numeric_limits<uint32_t>().max()) != vk::Result::eSuccess)
+	{
+		throw std::runtime_error("Failed to wait for fence");
+	}
+
+	vk::Result result = context->GetDevice().resetFences(1, &_swapchain->GetCurrentFrame()->GetInFlightFence());
+
+	if (result != vk::Result::eSuccess)
+	{
+		throw std::runtime_error("Failed to reset fence");
+	}
+
+	uint32 imageIndex;
+
+	try
+	{
+		imageIndex = context->GetDevice().acquireNextImageKHR(_swapchain->GetSwapchain(), std::numeric_limits<uint64_t>().max(), _swapchain->GetCurrentFrame()->GetImageAvailableSemaphore(), nullptr).value;
+	}
+	catch (vk::OutOfDateKHRError e)
+	{
+		_swapchain->Recreate();
+		return -1;
+	}
+
+	_swapchain->GetCurrentFrame()->GetCommandBuffer().reset();
+
+	vk::CommandBufferBeginInfo beginInfo = {};
+	_swapchain->GetCurrentFrame()->GetCommandBuffer().begin(beginInfo);
+
+	return imageIndex;
+}
+
+void cp::RendererPrototype::SubmitFrame(cp::Swapchain* _swapchain)
+{
+	_swapchain->GetCurrentFrame()->GetCommandBuffer().end();
+
+	vk::SubmitInfo submitInfo = {};
+	vk::Semaphore waitSemaphores[] = { _swapchain->GetCurrentFrame()->GetImageAvailableSemaphore() };
+	vk::PipelineStageFlags waitStages[] = { vk::PipelineStageFlagBits::eColorAttachmentOutput };
+	vk::Semaphore signalSemaphores[] = { _swapchain->GetCurrentFrame()->GetRenderFinishedSemaphore() };
+	submitInfo.waitSemaphoreCount = 1;
+	submitInfo.pWaitSemaphores = waitSemaphores;
+	submitInfo.pWaitDstStageMask = waitStages;
+	submitInfo.commandBufferCount = 1;
+	submitInfo.pCommandBuffers = &_swapchain->GetCurrentFrame()->GetCommandBuffer();
+	submitInfo.signalSemaphoreCount = 1;
+	submitInfo.pSignalSemaphores = signalSemaphores;
+
+	vk::Queue graphicsQueue = context->GetDevice().getQueue(context->GetQueueFamilyIndices().graphicsFamily.value(), 0);
+
+	vk::Result result = graphicsQueue.submit(1, &submitInfo, _swapchain->GetCurrentFrame()->GetInFlightFence());
+
+	if (result != vk::Result::eSuccess)
+	{
+		throw std::runtime_error("Failed to submit command buffer");
+	}
+}
+
+void cp::RendererPrototype::PresentFrame(cp::Swapchain* _swapchain, uint32_t _index)
+{
+	vk::Result result = vk::Result::eSuccess;
+	vk::Queue graphicsQueue = context->GetDevice().getQueue(context->GetQueueFamilyIndices().graphicsFamily.value(), 0);
+	vk::Semaphore signalSemaphores[] = { _swapchain->GetCurrentFrame()->GetRenderFinishedSemaphore() };
+
+	vk::PresentInfoKHR presentInfo = {};
+	presentInfo.waitSemaphoreCount = 1;
+	presentInfo.pWaitSemaphores = signalSemaphores;
+	vk::SwapchainKHR swapchains[] = { _swapchain->GetSwapchain() };
+	presentInfo.swapchainCount = 1;
+	presentInfo.pSwapchains = swapchains;
+	presentInfo.pImageIndices = &_index;
+
+	try
+	{
+		result = graphicsQueue.presentKHR(presentInfo);
+	}
+	catch (vk::OutOfDateKHRError e)
+	{
+		_swapchain->Recreate();
+	}
+
+	if (result == vk::Result::eErrorOutOfDateKHR || result == vk::Result::eSuboptimalKHR)
+	{
+		_swapchain->Recreate();
+	}
+	else if (result != vk::Result::eSuccess)
+	{
+		throw std::runtime_error("Failed to present swapchain image");
+	}
+}
+
+void cp::RendererPrototype::EndFrame(cp::Swapchain* _swapchain)
+{
+	_swapchain->SetCurrentFrame((_swapchain->GetCurrentFrameIndex() + 1) % _swapchain->GetFrameCount());
+}
+
+cp::Renderpass& cp::RendererPrototype::RegisterRenderPass(const std::string& _name, vk::RenderPass _rp)
+{
+	if (renderPasses.find(_name) == renderPasses.end())
+	{
+		renderPasses.insert({ _name, cp::Renderpass(context, _name, _rp) });
+	}
+
+	return renderPasses.at(_name);
+}
+
+cp::Renderpass& cp::RendererPrototype::GetRenderPass(const std::string& _name)
+{
+	return renderPasses.at(_name);
+}
+
+std::vector<std::string> cp::RendererPrototype::GetRenderPassNames()
+{
+	std::vector<std::string> names;
+
+	for (auto& [name, _] : renderPasses)
+	{
+		names.push_back(name);
+	}
+
+	return names;
+}
---
+++ Core/src/Render/Renderer/RendererPrototype.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "../../pch.hpp"
+#include "../../Context/VulkanContext.hpp"
+
+#include "Renderpass.hpp"
+
+#include "../../Resources/Material.hpp"
+#include "../../Resources/MaterialInstance.hpp"
+#include "../../Resources/Mesh.hpp"
+
+namespace cp
+{
+	struct TransformData
+	{
+		glm::mat4 modelMatrix;
+		glm::mat4 normalMatrix;
+	};
+
+	struct InstanceGroup
+	{
+		cp::Material* material;
+		cp::MaterialInstance* materialInstance;
+		cp::Mesh* mesh;
+		std::vector<TransformData> transforms;
+		//uint32_t instanceOffset = 0;
+	};
+
+	class RendererPrototype
+	{
+		protected:
+			cp::VulkanContext* context = nullptr;
+			vk::RenderPass mainRenderPass = VK_NULL_HANDLE;
+			std::unordered_map<std::string, Renderpass> renderPasses;
+
+			virtual void CreateFixedPipelines();
+			virtual void CreateMainRenderPass() = 0;
+			virtual void CreateRenderPasses();
+			virtual uint32_t PrepareFrame(cp::Swapchain* _swapchain);
+			virtual void SubmitFrame(cp::Swapchain* _swapchain);
+			virtual void PresentFrame(cp::Swapchain* _swapchain, uint32_t _index);
+			virtual void EndFrame(cp::Swapchain* _swapchain);
+
+			virtual void RenderFrame(const std::vector<InstanceGroup>& _instanceGroups) = 0;
+
+		public:
+			RendererPrototype(cp::VulkanContext* _context) : context(_context) {}
+			virtual ~RendererPrototype() = default;
+
+			virtual void BuildRenderer();
+			virtual void CleanupRenderer();
+
+			virtual void Render(const std::vector<InstanceGroup>& _instanceGroups) = 0;
+
+			inline std::unordered_map<std::string, Renderpass>& GetRenderPasses() { return renderPasses; }
+			inline constexpr cp::VulkanContext* GetContext() { return context; }
+
+			Renderpass& RegisterRenderPass(const std::string& _name, vk::RenderPass _rp);
+			Renderpass& GetRenderPass(const std::string& _name);
+			std::vector<std::string> GetRenderPassNames();
+	};
+}
