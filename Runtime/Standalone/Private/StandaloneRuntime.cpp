b05a69e36da6a984bfc63e473a40888c02283fea
Aurélien "Epharos" REY <Neox30650@gmail.com>
2026-04-18 01:29:39 +0200

    Made the application load plugins

--- Runtime/Standalone/Private/StandaloneRuntime.cpp
+++ Runtime/Standalone/Private/StandaloneRuntime.cpp
@@ -1,7 +1,7 @@
 #include <Common/Core/Macros.hpp>
 #include <Common/Core/Log.hpp>
 #include <Common/Async/JobSystem.hpp>
-#include <Common/IO/FileHelper.hpp>
+#include <Common/Plugin/PluginHost.hpp>
 
 #include <RHI/RenderingHardwareInterface.hpp>
 #include <RHI/Core.hpp>
@@ -14,13 +14,15 @@
 #include <Resources/AssetRegistry.hpp>
 #include "../../../Rendering/Private/Renderer.hpp"
 
+#include <filesystem>
+
 int main(int argc, char** argv)
 {
     ////////////////////////////
     /// Setup Loggers
     ////////////////////////////
-    constexpr const char* InitLabel = "Init";
-    constexpr const char* CleanupLabel = "Cleanup";
+    constexpr auto InitLabel = "Init";
+    constexpr auto CleanupLabel = "Cleanup";
 
     const std::shared_ptr<cp::IMessageVisitor> messageVisitor = std::make_shared<cp::ConsoleVisitor>();
     const std::shared_ptr<cp::IMessageVisitor> fileMessageVisitor = std::make_shared<cp::FileVisitor>();
@@ -32,6 +34,68 @@ int main(int argc, char** argv)
     compositeLogger->AddLogger(fileLogger);
 
     compositeLogger->Log(CP_LOG_EVENT(cp::ILogger::Info, InitLabel, cp::Message::Create("Hello, World!")));
+
+    cp::RegistryManager registryManager;
+    registryManager.GetOrCreate<cp::IRenderPass>("Renderpass");
+
+    ////////////////////////////
+    /// Load Plugins
+    ////////////////////////////
+
+    cp::PluginHostContext pluginHostContext
+    {
+        .mainLogger = compositeLogger.get(),
+        .registryManager = &registryManager,
+    };
+
+    cp::PluginHost pluginHost { pluginHostContext };
+
+    const std::filesystem::path executableDirectory =
+        (argc > 0 && argv[0] != nullptr)
+        ? std::filesystem::absolute(argv[0]).parent_path()
+        : std::filesystem::current_path();
+    const std::filesystem::path pluginsDirectory = executableDirectory / "Plugins";
+
+    if (std::filesystem::exists(pluginsDirectory) && std::filesystem::is_directory(pluginsDirectory))
+    {
+        const size_t loadedPluginCount = pluginHost.LoadPluginsFromDirectory(pluginsDirectory);
+        if (loadedPluginCount > 0)
+        {
+            compositeLogger->Log(CP_LOG_EVENT(
+                cp::ILogger::Info,
+                InitLabel,
+                cp::Message::Create("Loaded {} plugin(s) from {}", loadedPluginCount, pluginsDirectory.string())
+            ));
+        }
+        else
+        {
+            const std::string& pluginError = pluginHost.GetLastError();
+            if (!pluginError.empty())
+            {
+                compositeLogger->Log(CP_LOG_EVENT(
+                    cp::ILogger::Warning,
+                    InitLabel,
+                    cp::Message::Create("No plugin loaded from {} ({})", pluginsDirectory.string(), pluginError)
+                ));
+            }
+            else
+            {
+                compositeLogger->Log(CP_LOG_EVENT(
+                    cp::ILogger::Info,
+                    InitLabel,
+                    cp::Message::Create("No plugins found in {}", pluginsDirectory.string())
+                ));
+            }
+        }
+    }
+    else
+    {
+        compositeLogger->Log(CP_LOG_EVENT(
+            cp::ILogger::Info,
+            InitLabel,
+            cp::Message::Create("Plugin directory not found: {}", pluginsDirectory.string())
+        ));
+    }
 
     ////////////////////////////
     /// Initialize JobSystem
@@ -91,10 +155,15 @@ int main(int argc, char** argv)
         .frameCount = 3,
         .extent = windowInfo.extent,
         .imageFormat = cp::Format::R8G8B8A8_UNORM,
-        .nativeWindowHandle = window.GetNativeWindowHandle()
+        .nativeWindowHandle = window.GetNativeWindowHandle(),
+        .registryManager = &registryManager
     };
 
     cp::Renderer renderer { renderInfo, *rhi };
+
+    renderer.AddFrameGraphPass("SceneRenderPass");
+    renderer.AddFrameGraphPass("NegativePostFX");
+    renderer.RecompileFrameGraph();
 
     ////////////////////////////
     /// Loop
@@ -112,6 +181,10 @@ int main(int argc, char** argv)
     ////////////////////////////
     /// Cleanup
     ////////////////////////////
+
+    renderer.ResetFrameGraph();
+    cp::AssetRegistry::Instance().Cleanup();
+
     compositeLogger->Log(CP_LOG_EVENT(
         cp::ILogger::Info,
         CleanupLabel,
@@ -125,6 +198,9 @@ int main(int argc, char** argv)
         CleanupLabel,
         cp::Message::Create("All background jobs completed")
     ));
+
+    pluginHost.UnloadAll();
+    compositeLogger->Log(CP_LOG_EVENT(cp::ILogger::Info, CleanupLabel, cp::Message::Create("Plugins unloaded")));
     
     cp::JobSystem::Shutdown();
     compositeLogger->Log(CP_LOG_EVENT(cp::ILogger::Info, CleanupLabel, cp::Message::Create("JobSystem shut down")));
